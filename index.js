fadeb1c9b1aab259aec563e0db6570fc288f8275
msscompany1 <m.soboh@bau.edu.lb>
2025-04-10 12:40:31 +0300

    Fix dates error wrike api in the array

--- index.js
+++ index.js
@@ -97,20 +97,23 @@ class WrikeBot extends TeamsActivityHandler {
       if (!title) throw new Error("Missing required field: title");
 
       const wrikeToken = process.env.WRIKE_ACCESS_TOKEN;
-        const wrikeResponse = await axios.post('https://www.wrike.com/api/v4/tasks', null, {
+        const wrikeResponse = await axios.post('https://www.wrike.com/api/v4/tasks', {
+          title,
+          status,
+          importance: 'High',
+          dates: {
+            start: startDate,
+            due: dueDate,
+          },
+          responsibles: [assignee],
+          parents: [location],
+        }, {
           headers: {
             Authorization: `Bearer ${wrikeToken}`,
+            'Content-Type': 'application/json',
           },
-          params: {
-            title,
-            status,
-            importance: 'High',
-            responsibles: assignee,
-            parents: location,
-            'dates[start]': startDate,
-            'dates[due]': dueDate,
-          }
         });
+      
       
       
       const taskLink = wrikeResponse.data.data[0].permalink;
